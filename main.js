c121224b277c817cfea973cef5e8abc446fb83c1
Barry Yu <doung1118@gmail.com>
2020-07-22 15:45:33 +0800

    1:first

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = document.querySelector("#app");
+const phone = document.querySelector(".phone");
+const selectBtn = document.querySelector(".select-btn");
+const submitBtn = document.querySelector("#submit");
+
+/////////////// 請繼續完成 ///////////////
+
+function createDropDown(e) {
+  let menu = document.querySelector(".select-menu");
+  // please continue .....
+}
+
+// 從這行 code 開始思考⋯⋯
+selectBtn.addEventListener("click", createDropDown);
